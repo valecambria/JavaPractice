19b3b9ad4bef99a27b361e257bb65ea50cc0d854
vale <vale.cambria@hotmail.com>
2023-04-12 14:23:36 -0300

    added new controllers

--- src/main/java/com/practica/Practica/controllers/PersonaController.java
+++ src/main/java/com/practica/Practica/controllers/PersonaController.java
@@ -9,55 +9,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin(origins = "*") //desde cualquier origen voy a poder acceder a los datos de mi API
 @RequestMapping(path="api/v1/personas")
-public class PersonaController {
-    private PersonaServiceImpl personaService;
+public class PersonaController extends BaseControllerImpl<Persona, PersonaServiceImpl> {
 
-    public PersonaController(PersonaServiceImpl personaService) {
-        this.personaService = personaService;
-    }
-
-    @GetMapping("")
-    public ResponseEntity<?> getAll(){
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaService.findAll());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getOne(@PathVariable Long id){
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaService.findById(id));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-    }
-
-    @PostMapping("")
-    public ResponseEntity<?> save(@RequestBody Persona entity){
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaService.save(entity));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Persona entity){
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaService.update(id, entity));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id){
-        try {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(personaService.delete(id));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-    }
 }
